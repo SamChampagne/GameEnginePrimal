f2009e4111659bbfb1bae84f3e549fabc156411d
SamChampagne <samolichampagne@icloud.com>
2024-04-04 17:29:17 -0400

    Création

---
+++ Primal/PrimalInterface/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using PrimalInterface.gameproject;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimalInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // événement Loaded (quand la page sera load, la méthode OnMainWindowLoaded se lance)
+            Loaded += OnMainWindowLoaded;
+        }
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // je désabonne l'événement loaded de la méthode comme sa elle se lance une fois et 
+            // je lance la méthode OpenProjectBroswerDialog
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectBrowserDialog();
+        }
+        private void OpenProjectBrowserDialog()
+        {
+            // créer une nouvelle instance du menu
+            var projectBrowser = new ProjectMenu();
+            // création d'un if en vérifiant si la boite est ouvert 
+            if(projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                
+            }
+
+        }
+    }
+}
---
+++ Primal/PrimalInterface/gameproject/ProjectMenu.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrimalInterface.gameproject
+{
+    /// <summary>
+    /// Logique d'interaction pour ProjectMenu.xaml
+    /// </summary>
+    public partial class ProjectMenu : Window
+    {
+        public ProjectMenu()
+        {
+            InitializeComponent();
+            openProjectButton.IsChecked = true;
+            newProjectButton.IsChecked = false;
+        }
+        private void OnToggle_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            // si l'objet qui a déclencher à été déclencher par openProjectButton
+            if(sender == openProjectButton)
+            {
+                if(newProjectButton.IsChecked == true)
+                {
+                    newProjectButton.IsChecked = false;
+                    Browser.Margin = new Thickness(0);
+                }
+                openProjectButton.IsChecked = true;
+            }
+            else
+            {
+                if (openProjectButton.IsChecked == true)
+                {
+                    openProjectButton.IsChecked = false;
+                    Browser.Margin = new Thickness(-800,0,0,0);
+                }
+                newProjectButton.IsChecked = true;
+            }
+            
+        }
+    }
+}
